6ed5d3a558eccd384608b0f105beb308d498a828
Sofija Dimitrijevic <dimitrijevic.sofijaa@gmail.com>
2018-03-25 13:32:27 +0200

    Dodata test klasa VuklanizerskaRadnjaTest. Testirane metode klase VulkanizerskaRadnja. Uvedene izmene u metode.

--- VulkanizerskaRadnja/src/gume_radnja/VulkanizerskaRadnja.java
+++ VulkanizerskaRadnja/src/gume_radnja/VulkanizerskaRadnja.java
@@ -32,7 +32,7 @@ public class VulkanizerskaRadnja {
 			throw new NullPointerException("Guma ne sme biti null");
 		if (gume.contains(a))
 			throw new RuntimeException("Guma vec postoji");
-		gume.addFirst(a);
+		gume.add(a);
 	}
 
 	/**
@@ -49,7 +49,7 @@ public class VulkanizerskaRadnja {
 			return null;
 		LinkedList<AutoGuma> novaLista = new LinkedList<AutoGuma>();
 		for (int i = 0; i < gume.size(); i++)
-			if (gume.get(i).equals(markaModel))
+			if (gume.get(i).getMarkaModel().equals(markaModel))
 				novaLista.add(gume.get(i));
 		return novaLista;
 	}
---
+++ VulkanizerskaRadnja/test/gume_radnja_test/VulkanizerskaRadnjaTest.java
@@ -0,0 +1,136 @@
+/**
+ * 
+ */
+package gume_radnja_test;
+
+import static org.junit.Assert.*;
+
+import java.util.LinkedList;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import gume.AutoGuma;
+import gume_radnja.VulkanizerskaRadnja;
+
+/**
+ * @author Sofija Dimitrijevic
+ *
+ */
+public class VulkanizerskaRadnjaTest {
+	private VulkanizerskaRadnja vr;
+	private AutoGuma a;
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+		vr = new VulkanizerskaRadnja();
+
+		a = new AutoGuma();
+		a.setMarkaModel("Aurora");
+		a.setPrecnik(20);
+		a.setSirina(200);
+		a.setVisina(80);
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+		vr = null;
+		a = null;
+	}
+
+	/**
+	 * Test method for
+	 * {@link gume_radnja.VulkanizerskaRadnja#dodajGumu(gume.AutoGuma)}.
+	 */
+	@Test
+	public void testDodajGumu() {
+		vr.dodajGumu(a);
+		assertEquals(1, vr.pronadjiGumu(a.getMarkaModel()).size());
+		assertEquals(a, vr.pronadjiGumu(a.getMarkaModel()).get(0));
+	}
+
+	/**
+	 * Test method for
+	 * {@link gume_radnja.VulkanizerskaRadnja#dodajGumu(gume.AutoGuma)}.
+	 */
+	@Test (expected = java.lang.NullPointerException.class)
+	public void testDodajGumuNull() {
+		vr.dodajGumu(null);
+
+	}
+	/**
+	 * Test method for
+	 * {@link gume_radnja.VulkanizerskaRadnja#dodajGumu(gume.AutoGuma)}.
+	 */
+	@Test (expected = java.lang.RuntimeException.class)
+	public void testDodajGumuDuplikat() {
+		vr.dodajGumu(a);
+		AutoGuma a2 = new AutoGuma();
+		a2.setMarkaModel("Aurora");
+		a2.setPrecnik(20);
+		a2.setSirina(200);
+		a2.setVisina(80);
+		vr.dodajGumu(a2);
+
+	}
+	/**
+	 * Test method for
+	 * {@link gume_radnja.VulkanizerskaRadnja#pronadjiGumu(java.lang.String)}.
+	 */
+	@Test
+	public void testPronadjiGumu() {
+		assertEquals(null,vr.pronadjiGumu(null));
+	}
+	
+	/**
+	 * Test method for
+	 * {@link gume_radnja.VulkanizerskaRadnja#pronadjiGumu(java.lang.String)}.
+	 */
+	@Test
+	public void testPronadjiGumuNePostoji() {
+		vr.dodajGumu(a);
+		LinkedList<AutoGuma> ag = new LinkedList<AutoGuma>();
+		assertEquals(ag,vr.pronadjiGumu("Avon"));
+	}
+	/**
+	 * Test method for
+	 * {@link gume_radnja.VulkanizerskaRadnja#pronadjiGumu(java.lang.String)}.
+	 */
+	@Test
+	public void testPronadjiGumuPostojiJedna() {
+		vr.dodajGumu(a);
+		LinkedList<AutoGuma> ag = new LinkedList<AutoGuma>();
+		ag.add(a);
+		assertEquals(ag,vr.pronadjiGumu(a.getMarkaModel()));
+	}
+	/**
+	 * Test method for
+	 * {@link gume_radnja.VulkanizerskaRadnja#pronadjiGumu(java.lang.String)}.
+	 */
+	@Test
+	public void testPronadjiGumuPostojeDve() {
+		vr.dodajGumu(a);
+		
+		AutoGuma a2 = new AutoGuma();
+		a2.setMarkaModel("Aurora");
+		a2.setPrecnik(21);
+		a2.setSirina(201);
+		a2.setVisina(81);
+		vr.dodajGumu(a2);
+		
+		LinkedList<AutoGuma> ag = new LinkedList<AutoGuma>();
+		ag.add(a);
+		ag.add(a2);
+		
+		assertEquals(ag, vr.pronadjiGumu(a.getMarkaModel()));
+	}
+	
+
+}
